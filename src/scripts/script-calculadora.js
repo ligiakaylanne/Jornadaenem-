0c469596a13309b7818279a253234e9ed6db49fa
Gabriel Evangelista <evangelistagabriel798@gmail.com>
2024-08-24 10:11:58 -0300

    calculadora de notas concluida

--- src/scripts/script-calculadora.js
+++ src/scripts/script-calculadora.js
@@ -1,4 +1,5 @@
 function calculateScore() {
+    
     // Obtém os valores dos inputs
     const linguagens = parseFloat(document.getElementById('linguagens').value);
     const matematica = parseFloat(document.getElementById('matematica').value);
@@ -12,9 +13,17 @@ function calculateScore() {
         return;
     }
 
-    // Calcula a nota total (simples média aritmética)
-    const totalScore = (linguagens + matematica + natureza + humanas + redacao) / 5;
+    //verifica se todas as notas sao >= 0 e <= 1000, caso sim faz a operacao
+    if(linguagens >= 0 && linguagens <= 1000 && matematica >= 0 && matematica <= 1000 && natureza >= 0 && natureza <= 1000 && humanas >= 0 && humanas <= 1000 && redacao >= 0 && redacao <= 1000){
 
-    // Exibe o resultado
-    document.getElementById('result').textContent = `Sua nota total é: ${totalScore.toFixed(2)}`;
+        // Calcula a nota total (simples média aritmética)
+        const totalScore = (linguagens + matematica + natureza + humanas + redacao) / 5;
+
+        // Exibe o resultado
+        document.getElementById('result').textContent = `Sua nota total é: ${totalScore.toFixed(2)}`;
+        return;
+    }
+    
+    alert("Notas inválidas, apenas são permitidas notas de 0 a 1000");
+    return;
 }
